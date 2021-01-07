9aeac9ea5f704996054edafa9846968544eaaf76
ferm1n <fgicangao25@gmail.com>
2021-01-07 14:51:54 -0800

    final_output

---
+++ src/components/App.jsx
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+
+
+
+class App extends Component {
+  state = {};
+
+  render() {
+    return <div>
+
+      <table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">Author</th>
+      <th scope="col">Title</th>
+      <th scope="col">Description</th>
+      <th scope="col">Price</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th>"   "</th>
+      <td>You Are Your Only Limit</td>
+      <td>This is a inspiring Book</td>
+      <td>300</td>
+      <td>Reserve</td>
+    </tr>
+    <tr>
+      <th>Jobathan A. Levi </th>
+      <td>The Only Skill That Matter</td>
+      <td>This is a inspiring Book</td>
+      <td>350</td>
+      <td>Reserve</td>
+    </tr>
+    <tr>
+      <th>R.C Allen</th>
+      <td>Inspirational Qoutes</td>
+      <td>This is a inspiring Book</td>
+      <td>400</td>
+      <td>Reserve</td>
+    </tr>
+    <tr>
+      <th>Darleen Mitchel</th>
+      <td>The Secret Of Success</td>
+      <td>This is a inspiring Book</td>
+      <td>500</td>
+      <td>Reserve</td>
+    </tr>
+    <tr>
+      <th>Katherine S. Pratt</th>
+      <td>The Gift of Forgiveness</td>
+      <td>This is a inspiring Book</td>
+      <td>550</td>
+      <td>Reserve</td>
+    </tr>
+    
+  </tbody>
+</table>
+
+ </div>;
+ 
+    
+  }
+}
+
+export default App;
